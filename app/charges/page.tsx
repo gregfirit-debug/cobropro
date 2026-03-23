6c8db07d03c74fef8d40525186027868617e1e56
Roberto <robertofilluelo@hotmail.com>
2026-03-23 09:33:09 -0300

    force dynamic pages with prisma

--- app/charges/page.tsx
+++ app/charges/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic"
 import Link from "next/link"
 import { DeleteChargeButton } from "@/components/delete-charge-button"
 import { prisma } from "@/lib/prisma"
--- app/clients/new/page.tsx
+++ app/clients/new/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic"
 import { createClient } from "./actions"
 
 export default function NewClientPage() {
