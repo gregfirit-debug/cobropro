b38a630ba0e0b385f76c0438a97c60dc2106d886
Roberto <robertofilluelo@hotmail.com>
2026-03-26 11:21:13 -0300

    fix middleware size

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,5 @@
 export { auth as middleware } from "@/auth"
 
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/clients/:path*",
-    "/charges/:path*",
-    "/account/:path*",
-  ],
+  matcher: ["/dashboard", "/clients", "/charges"],
 }
