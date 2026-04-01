f79966c4a5ec9c56aea2cbb0f6243c158ba52bdb
Roberto <robertofilluelo@hotmail.com>
2026-03-31 22:34:09 -0300

    fix PWA registration

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
-import withPWA from "next-pwa"
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-export default withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-})(nextConfig)
+export default withPWA(nextConfig);
