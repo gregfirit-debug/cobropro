9bc35b62a8b76cb62a2ca3ede33eb9c821c71850
Roberto <robertofilluelo@hotmail.com>
2026-03-31 23:25:40 -0300

    fix manifest icons purpose

--- app/manifest.ts
+++ app/manifest.ts
@@ -14,13 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
       src: "/icon-192-v2.png",  
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable",
+        purpose: "any",
       },
       {
         src: "/icon-512-v2.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "maskable",
+        purpose: "any",
       },
     ],
   }
