2e10df4958a1e6a74cc1ac171bf456b2a3ef7886
Roberto <robertofilluelo@hotmail.com>
2026-03-31 22:53:50 -0300

    change PWA package

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import withPWAInit from "next-pwa";
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
   register: true,
-  skipWaiting: true,
+  reloadOnOnline: true,
   disable: process.env.NODE_ENV === "development",
 });
 
