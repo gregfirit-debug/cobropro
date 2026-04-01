2e0b5c8a55beedc6e36c9bf3fe916b200ae5cfcf
Roberto <robertofilluelo@hotmail.com>
2026-03-31 22:58:47 -0300

    force SW control

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   register: true,
+  skipWaiting: true,
+  clientsClaim: true,
   reloadOnOnline: true,
   disable: process.env.NODE_ENV === "development",
 });
