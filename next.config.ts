8c8245f64f8e59b7d31e093ea50cfeaf5e1b6ac5
Roberto <robertofilluelo@hotmail.com>
2026-03-31 23:01:08 -0300

    fix workbox options

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,12 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   register: true,
-  skipWaiting: true,
-  clientsClaim: true,
-  reloadOnOnline: true,
   disable: process.env.NODE_ENV === "development",
+  reloadOnOnline: true,
+  workboxOptions: {
+    skipWaiting: true,
+    clientsClaim: true,
+  },
 });
 
 const nextConfig = {
